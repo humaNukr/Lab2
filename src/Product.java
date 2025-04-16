d35d679e313a4192051efd338728a7307d90208a
stasyzar <zarovskaanastasia@gmail.com>
2025-04-16 21:24:49 +0300

    Add Product class

---
+++ src/Product.java
@@ -0,0 +1,86 @@
+public class Product {
+    private String name;
+    private String description;
+    private String manufacturer;
+    private int quantity;
+    private double price;
+    private String groupName;
+
+    public Product(String name, String description, String manufacturer, int quantity, double price, String groupName) {
+        this.name = name;
+        this.description = description;
+        this.manufacturer = manufacturer;
+        this.quantity = quantity;
+        this.price = price;
+        this.groupName = groupName;
+    }
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void addQuantity(int amount) {
+        this.quantity += amount;
+    }
+
+    public boolean removeQuantity(int amount) {
+        if (amount <= this.quantity) {
+            this.quantity -= amount;
+            return true;
+        }
+        return false;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    public double getTotalValue() {
+        return quantity * price;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
